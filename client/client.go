a095fec25af13b182d56cb41f887efb0e0a6fc08
fredal <1271123275@qq.com>
2017-12-27 23:08:26 +0800

    client connect/handle resp

--- client/client.go
+++ client/client.go
@@ -1,1 +1,137 @@
 package client
+
+import (
+	"sync"
+	"bufio"
+	"frpc/protocol"
+	"time"
+	"net"
+	"frpc/log"
+	"rpcx/util"
+	"frpc/core"
+	"errors"
+	"io"
+)
+
+type ServiceError string
+
+func (e ServiceError) Error() string {
+	return string(e)
+}
+
+var (
+	ErrShutdown         = errors.New("connection is shut down")
+	ErrUnsupportedCodec = errors.New("unsupported codec")
+)
+
+type Client struct {
+	option   Option
+	reqMutex sync.Mutex // protects following
+	r        *bufio.Reader
+	conn     net.Conn
+	mutex    sync.Mutex // protects following
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool // user has called Close
+	shutdown bool // server has told us to stop
+}
+
+type Option struct {
+	SerializeType     protocol.SerializeType
+	CompressType      protocol.CompressType
+	ReadTimeout       time.Duration
+	WriteTimeout      time.Duration
+	ConnTimeout       time.Duration
+	Heartbeat         bool
+	HeartbeatInterval time.Duration
+}
+
+type Call struct {
+	ServicePath   string
+	ServiceMethod string            // The name of the service and method to call.
+	Metadata      map[string]string //metadata
+	ResMetadata   map[string]string
+	Args          interface{} // The argument to the function (*struct).
+	Reply         interface{} // The reply from the function (*struct).
+	Error         error       // After completion, the error status.
+	Done          chan *Call
+	Raw           bool
+}
+
+func (call *Call) done() {
+	select {
+	case call.Done <- call:
+		// ok
+	default:
+		log.Debug("rpc: discarding Call reply due to insufficient Done chan capacity")
+	}
+}
+
+func (client *Client) handleResponse(reader io.Reader) {
+	var err error
+	var res = protocol.NewMessage()
+	for err == nil {
+		err := res.Decode(reader)
+		if err != nil {
+			break
+		}
+		seq := res.Seq()
+		client.mutex.Lock()
+		call := client.pending[seq]
+		delete(client.pending, seq)
+		client.mutex.Unlock()
+
+		switch {
+		case call == nil:
+		case res.MessageStatusType() == protocol.Error:
+			call.Error = ServiceError(res.Metadata[protocol.ServiceError])
+			call.ResMetadata = res.Metadata
+			//
+			call.done()
+		default:
+			data := res.Payload
+			if len(data) > 0 {
+				if res.CompressType() == protocol.Gzip {
+					data, err = util.Unzip(data)
+					if err != nil {
+					}
+					call.Error = ServiceError("unzip payload: " + err.Error())
+				}
+				codec := core.Codecs[res.SerializeType()]
+				if codec == nil {
+					call.Error = ServiceError(ErrUnsupportedCodec.Error())
+				} else {
+					err = codec.Decode(data, call.Reply)
+					if err != nil {
+						call.Error = ServiceError(err.Error())
+					}
+				}
+			}
+			call.ResMetadata = res.Metadata
+			call.done()
+		}
+		res.Reset()
+	}
+	client.mutex.Lock()
+	client.shutdown = true
+	closing := client.closing
+	if err == io.EOF {
+		if closing {
+			err = ErrShutdown
+		} else {
+			err = io.ErrUnexpectedEOF
+		}
+	}
+	for _, call := range client.pending {
+		call.Error = err
+		call.done()
+	}
+	client.mutex.Unlock()
+	if err != nil && err != io.EOF && !closing {
+		log.Error("rpcx: client protocol error:", err)
+	}
+}
+
+func (client *Client) heartbeat() {
+
+}
--- client/connection.go
+++ client/connection.go
@@ -1,1 +1,72 @@
 package client
+
+import (
+	"net"
+	"time"
+	"bufio"
+	"frpc/log"
+)
+
+const (
+	// ReaderBuffsize is used for bufio reader.
+	ReaderBuffsize = 16 * 1024
+	// WriterBuffsize is used for bufio writer.
+	WriterBuffsize = 16 * 1024
+)
+
+func (c *Client) Connect(network, address string) error {
+	var conn net.Conn
+	var err error
+	switch network {
+	case "http":
+		conn, err = newHttpConn(c, network, address)
+	case "kcp":
+		conn, err = newKcpConn(c, network, address)
+	case "quic":
+		conn, err = newQuicConn(c, network, address)
+	default: //tcp unix udp
+		conn, err = newDirectConn(c, network, address)
+	}
+
+	if err == nil && conn != nil {
+		if c.option.ReadTimeout != 0 {
+			conn.SetReadDeadline(time.Now().Add(c.option.ReadTimeout))
+		}
+		if c.option.WriteTimeout != 0 {
+			conn.SetWriteDeadline(time.Now().Add(c.option.WriteTimeout))
+		}
+		c.conn = conn
+		reader := bufio.NewReaderSize(conn, ReaderBuffsize)
+		go c.handleResponse(reader)
+
+		if c.option.Heartbeat && c.option.HeartbeatInterval > 0 {
+			go c.heartbeat()
+		}
+	}
+	return err
+}
+
+func newHttpConn(client *Client, network string, address string) (net.Conn, error) {
+	conn, err := net.DialTimeout("tcp", address, client.option.ConnTimeout)
+	if err != nil {
+		log.Errorf("failed to dial server: %v", err)
+		return nil, err
+	}
+	//io.WriteString(conn, "CONNECT "+path+" HTTP/1.0\n\n"
+	return conn, nil
+}
+func newKcpConn(client *Client, network string, address string) (net.Conn, error) {
+	panic(client)
+}
+func newQuicConn(client *Client, network string, address string) (net.Conn, error) {
+	panic(client)
+}
+
+func newDirectConn(client *Client, network string, address string) (net.Conn, error) {
+	conn, err := net.DialTimeout(network, address, client.option.ConnTimeout)
+	if err != nil {
+		log.Errorf("failed to dial server: %v", err)
+		return nil, err
+	}
+	return conn, nil
+}

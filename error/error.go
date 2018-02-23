653c5138754055af0b363684a83d5aaec0dc54fb
fredal <1271123275@qq.com>
2018-02-23 16:26:53 +0800

    finish etc registry and discovery (random selector)

--- error/error.go
+++ error/error.go
@@ -1,1 +1,15 @@
 package error
+
+import "fmt"
+
+type MultiError struct {
+	Errors []error
+}
+
+func (e *MultiError) Error() string {
+	return fmt.Sprintf("%v", e.Errors)
+}
+
+func NewMultiError(errors []error) *MultiError {
+	return &MultiError{Errors: errors}
+}

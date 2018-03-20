5f52fd67bcdcec2d52e106b6fc8698a1ba5e4ee3
fredal <1271123275@qq.com>
2018-03-20 22:27:01 +0800

    selector test

--- test/selector_test.go
+++ test/selector_test.go
@@ -10,7 +10,9 @@ import (
 )
 
 func TestRandomSelector(t *testing.T) {
-	initTwoServer()
+	s1, s2 := initTwoServer()
+	defer s1.Close()
+	defer s2.Close()
 
 	client := client.NewClient().
 		Discovery(core.Consul, "/rpcx_test", "Arith", []string{"localhost:32787"}).
@@ -36,7 +38,9 @@ func TestRandomSelector(t *testing.T) {
 
 func TestRoundRobinSelector(t *testing.T) {
 
-	initTwoServer()
+	s1, s2 := initTwoServer()
+	defer s1.Close()
+	defer s2.Close()
 
 	client := client.NewClient().
 		Discovery(core.Consul, "/rpcx_test", "Arith", []string{"localhost:32787"}).
@@ -60,10 +64,11 @@ func TestRoundRobinSelector(t *testing.T) {
 
 }
 
-
 func TestHashSelector(t *testing.T) {
 
-	initTwoServer()
+	s1, s2 := initTwoServer()
+	defer s1.Close()
+	defer s2.Close()
 
 	client := client.NewClient().
 		Discovery(core.Consul, "/rpcx_test", "Arith", []string{"localhost:32787"}).
@@ -87,7 +92,91 @@ func TestHashSelector(t *testing.T) {
 
 }
 
-func initTwoServer() {
+
+func TestWeightedSelector(t *testing.T) {
+
+	s1, s2 := initTwoWeightedServer()
+	defer s1.Close()
+	defer s2.Close()
+
+	client := client.NewClient().
+		Discovery(core.Consul, "/rpcx_test", "Arith", []string{"localhost:32787"}).
+		Selector(core.Weighted)
+
+	defer client.Close()
+
+	args := &Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &Reply{}
+	for i := 0; i < 10; i++ {
+		err := client.CallProxy(context.Background(), "Mul", args, reply)
+		if err != nil {
+			t.Fatalf("failed to call: %v", err)
+		}
+		println(reply.C)
+	}
+}
+
+func TestPingSelector(t *testing.T) {
+
+	s1, s2 := initTwoServer()
+	defer s1.Close()
+	defer s2.Close()
+
+	client := client.NewClient().
+		Discovery(core.Consul, "/rpcx_test", "Arith", []string{"localhost:32787"}).
+		Selector(core.Ping)
+
+	defer client.Close()
+
+	args := &Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &Reply{}
+	for i := 0; i < 10; i++ {
+		err := client.CallProxy(context.Background(), "Mul", args, reply)
+		if err != nil {
+			t.Fatalf("failed to call: %v", err)
+		}
+		println(reply.C)
+	}
+
+}
+
+func TestGeoSelector(t *testing.T) {
+
+	s1, s2 := initTwoServer()
+	defer s1.Close()
+	defer s2.Close()
+
+	client := client.NewClient().
+		Discovery(core.Consul, "/rpcx_test", "Arith", []string{"localhost:32787"}).
+		Selector(core.Closest)
+
+	defer client.Close()
+
+	args := &Args{
+		A: 10,
+		B: 20,
+	}
+
+	reply := &Reply{}
+	for i := 0; i < 10; i++ {
+		err := client.CallProxy(context.Background(), "Mul", args, reply)
+		if err != nil {
+			t.Fatalf("failed to call: %v", err)
+		}
+		println(reply.C)
+	}
+
+}
+
+func initTwoServer() (*server.Server, *server.Server) {
 	s1, _ := server.
 		NewServer().
 		Registry(core.Consul, "/rpcx_test", "tcp@localhost:8972", []string{"localhost:32787"}, time.Minute).
@@ -95,19 +184,37 @@ func initTwoServer() {
 
 	go s1.ServeProxy()
 
-	defer s1.Close()
-
 	time.Sleep(500 * time.Millisecond)
-
 
 	s2, _ := server.
 		NewServer().
-		Registry(core.Consul, "/rpcx_test", "tcp@localhost:8974", []string{"localhost:32787"}, time.Minute).
+		Registry(core.Consul, "/rpcx_test", "tcp@localhost:8973", []string{"localhost:32787"}, time.Minute).
 		RegisterName(new(Arith2), "Arith", "")
 
 	go s2.ServeProxy()
 
-	defer s2.Close()
+	time.Sleep(500 * time.Millisecond)
+	return s1, s2
+}
+
+
+func initTwoWeightedServer() (*server.Server, *server.Server) {
+	s1, _ := server.
+		NewServer().
+		Registry(core.Consul, "/rpcx_test", "tcp@localhost:8972", []string{"localhost:32787"}, time.Minute).
+		RegisterName(new(Arith), "Arith", "weight=1")
+
+	go s1.ServeProxy()
 
 	time.Sleep(500 * time.Millisecond)
+
+	s2, _ := server.
+		NewServer().
+		Registry(core.Consul, "/rpcx_test", "tcp@localhost:8973", []string{"localhost:32787"}, time.Minute).
+		RegisterName(new(Arith2), "Arith", "weight=7")
+
+	go s2.ServeProxy()
+
+	time.Sleep(500 * time.Millisecond)
+	return s1, s2
 }
